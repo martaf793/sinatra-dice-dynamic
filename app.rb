594a53b631d932e0d80ecba595a32db188f8ef8c
martaf793 <martaf@uchicago.edu>
2025-04-13 23:05:42 +0000

    dynamic dice works

--- app.rb
+++ app.rb
@@ -52,3 +52,14 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:n1/:n2") do
+  @n1_value=params.fetch("n1").to_i
+  @n2_value=params.fetch("n2").to_i
+  @rolls=[]
+  @n1_value.times do
+    n=rand(1..@n2_value)
+    @rolls.push(n)
+  end
+  erb(:flexible)
+end 
