eb2f579dff93a11ced788b2250a36e03b7444b86
martaf793 <martaf@uchicago.edu>
2025-04-13 23:07:24 +0000

    refactoring

--- app.rb
+++ app.rb
@@ -5,54 +5,6 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
 get("/dice/:n1/:n2") do
   @n1_value=params.fetch("n1").to_i
   @n2_value=params.fetch("n2").to_i
